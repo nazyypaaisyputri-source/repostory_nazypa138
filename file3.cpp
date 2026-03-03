44c7d8c49e8196fbe6f1b0aace65b735fa974759
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-03 22:17:16 +0700

    implementasi int main dan integrasi semua fungsi

--- file3.cpp
+++ file3.cpp
@@ -25,3 +25,9 @@ void output(){
     cout << "Hasilnya = " << luasPersegiP(p, l) << endl;
     cout << "terimakasih" << endl;
 }
+
+int main() { //mulai
+    input();
+    output();
+    cout << "hasil penjumlahan= " << jumlah(15.1, 24.9, 67);
+} //selesai
